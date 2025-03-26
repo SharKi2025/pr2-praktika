c1db9938ad660386df111991aca2ceca694c6203
SharKi098 <nikita162205@gmail.com>
2025-03-26 20:08:35 +0300

    первый коммит

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+
+class Calc
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Введите 1ое число");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите 2ое число:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию (+, -, *, /, ^):");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                default:
+                    throw new InvalidOperationException("Неверная операция");
+            }
+
+            Console.WriteLine($"Результат операции: {result}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Некорректный ввод числа");
+        }
+        
+        
+    }
+
+}
+
+
+
+
+
+
